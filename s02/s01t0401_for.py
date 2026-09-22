b671c8536b175cb8e4945edcfb188f23dfe0db4e
AdamRey22 <rodriguezreyrubi@gmail.com>
2026-09-22 16:40:59 +0000

    ⭐s01t0401 Finaliza toma de tiempo de la suma de 100 numeros usando un for

--- s02/s01t0401_for.py
+++ s02/s01t0401_for.py
@@ -15,11 +15,11 @@ timestamp_01 = time.time ()
 # de los "n" numeros naturales
 
 n = 100
-sum = 0
+total_sum = 0
 
 # Ciclo for, por cada de numero que hay de n+1 es imprimir
 for number in range(1, n+1):
-    sum = sum + number
+    total_sum = total_sum + number
     #la primera vez que se ejecuta el ciclo
     #1: sum < - 0 + 1
     # sum = 1
@@ -30,9 +30,12 @@ for number in range(1, n+1):
     #100: sum <- antSum_(-1) + 100
     #Asi tenemos los numeros sumados hasta 100
     #string permite combinar una salida con las variables 
-print(f"La suma de los números del 1 al {n} es : {sum}")
+print(f"La suma de los números del 1 al {n} es : {total_sum}")
 #formato f, string con F, combinar los valores con f
 #Tomando el tiempo final 
 timestamp_02 = time.time ()
+
+elapsed_time = (timestamp_02-timestamp_01) * 1e6
+
 #Impresion del tiempo de ejecusion
-print(f"Tiempo de ejecucion: {(timestamp_02-timestamp_01) * 1e6} μsegundos")
+print(f"Tiempo de ejecucion: {(timestamp_02-timestamp_01) * 1e6:.2f} μsegundos")
